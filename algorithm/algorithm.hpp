4af9d2355ffd95d8c684f3b31e3f70ae4687dc16
Gianmaria Piergianni <g.piergianni@student.tudelft.nl>
2026-07-08 17:20:22 +0200

    added header

---
+++ algorithm/algorithm.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "../plustimes/plustimes.hpp"
+#include <boost/multiprecision/cpp_int.hpp>
+#include <cstddef>
+#include <cstdint>
+#include <optional>
+
+using boost::multiprecision::cpp_int;
+
+void assertReadyForFPRAS(const PlusTimesProgram& P);
+
+double countCore(
+    const PlusTimesProgram& P,
+    std::size_t ns,
+    std::size_t nt,
+    cpp_int theta,
+    double kappa);
+
+double countCoreWithSupportThreshold(
+    const PlusTimesProgram& P,
+    std::size_t ns,
+    std::size_t nt,
+    cpp_int theta,
+    double kappa,
+    std::size_t supportThreshold);
+
+double counter(const PlusTimesProgram& P, double epsilon, double delta);
+
+std::optional<std::size_t> supportSizeBounded(
+    const PlusTimesProgram& P,
+    NodeId o,
+    std::size_t threshold);
+
+void seedSamplingForTesting(std::uint32_t seed);
